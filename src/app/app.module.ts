52cdb3cf35536ae787ea8bc5cfc5e9ad2d52128a
Sébastien Pertus <spertus@microsoft.com>
2017-04-04 13:34:50 +0200

    demo 04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RoomComponent } from './room/room.component';
 import { RoomService } from './room.service';
 
 import { VenueApi, BASE_PATH } from './ms/index';
+import { ScheduleComponent } from './schedule/schedule.component';
+
+var routes: Routes = [
+  { path: '', redirectTo: '/schedules', pathMatch: 'full' },
+  { path: "schedules", component: ScheduleComponent },
+  { path: "rooms", component: RoomComponent }
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoomComponent
+    RoomComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     RoomService,
